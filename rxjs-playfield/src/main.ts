bffd42a2f39786a669422e356042cf2fffddad55
Buwaneka Saranga <bsaranga@gmail.com>
2024-04-25 21:57:17 +0530

    Refactor click event subscription in main.ts

--- rxjs-playfield/src/main.ts
+++ rxjs-playfield/src/main.ts
@@ -1,5 +1,14 @@
 import { fromEvent } from "rxjs";
 
-fromEvent(document, "click").subscribe(() => {
-  console.log("Clicked!");
+let count = 0;
+let limit = 5;
+const observer = fromEvent(document, "click");
+
+const sub = observer.subscribe(() => {
+  count++;
+  console.log(count);
+  if (count === limit) {
+    console.log("Clicked 5 times");
+    sub.unsubscribe();
+  }
 });
