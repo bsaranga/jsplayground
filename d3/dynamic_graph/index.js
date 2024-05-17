cee19b3820a7dceec6d6e74fb48871cd54ff9515
Buwaneka Saranga <bsaranga@gmail.com>
2024-05-17 09:14:05 +0530

    Add new files and modify existing files for dynamic graph visualization in d3.js

--- d3/dynamic_graph/index.js
+++ d3/dynamic_graph/index.js
@@ -1,100 +1,177 @@
+
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
-const groups = {
-	"0": "mammal",
-	"1": "insect",
-	"2": "fish"
-}
+import getLinkColor from './utils/getLinkColor.js'
+import getNodeColor from './utils/getNodeColor.js'
+import getTextColor from './utils/getTextColor.js'
+import getNeighbors from './utils/getNeighbors.js'
+import isNeighborLink from './utils/isNeighborLink.js'
 
-const nodes = [
-	{ "id": "mammal", "group": 0, "label": "Mammals", "level": 1 },
-	{ "id": "dog", "group": 0, "label": "Dogs", "level": 2 },
-	{ "id": "cat", "group": 0, "label": "Cats", "level": 2 },
-	{ "id": "fox", "group": 0, "label": "Foxes", "level": 2 },
-	{ "id": "elk", "group": 0, "label": "Elk", "level": 2 },
-	{ "id": "insect", "group": 1, "label": "Insects", "level": 1 },
-	{ "id": "ant", "group": 1, "label": "Ants", "level": 2 },
-	{ "id": "bee", "group": 1, "label": "Bees", "level": 2 },
-	{ "id": "fish", "group": 2, "label": "Fish", "level": 1 },
-	{ "id": "carp", "group": 2, "label": "Carp", "level": 2 },
-	{ "id": "pike", "group": 2, "label": "Pikes", "level": 2 }
-]
+import baseNodes from './data/nodes.js'
+import baseLink from './data/links.js'
 
-const links = [
-	{ "target": "mammal", "source": "dog", "strength": 0.7 },
-	{ "target": "mammal", "source": "cat", "strength": 0.7 },
-	{ "target": "mammal", "source": "fox", "strength": 0.7 },
-	{ "target": "mammal", "source": "elk", "strength": 0.7 },
-	{ "target": "insect", "source": "ant", "strength": 0.7 },
-	{ "target": "insect", "source": "bee", "strength": 0.7 },
-	{ "target": "fish", "source": "carp", "strength": 0.7 },
-	{ "target": "fish", "source": "pike", "strength": 0.7 },
-	{ "target": "cat", "source": "elk", "strength": 0.1 },
-	{ "target": "carp", "source": "ant", "strength": 0.1 },
-	{ "target": "elk", "source": "bee", "strength": 0.1 },
-	{ "target": "dog", "source": "cat", "strength": 0.1 },
-	{ "target": "fox", "source": "ant", "strength": 0.1 },
-	{ "target": "pike", "source": "dog", "strength": 0.1 }
-]
+const nodes = [...baseNodes]
+const links = [...baseLink]
 
-const width = window.innerWidth;
-const height = window.innerHeight;
+const width = window.innerWidth
+const height = window.innerHeight
 
-const svg = d3.select("svg").attr("width", width).attr("height", height);
+const svg = d3.select('svg')
+svg.attr('width', width).attr('height', height)
+
+let linkElements,
+  nodeElements,
+  textElements
+
+// we use svg groups to logically group the elements together
+const linkGroup = svg.append('g').attr('class', 'links')
+const nodeGroup = svg.append('g').attr('class', 'nodes')
+const textGroup = svg.append('g').attr('class', 'texts')
+
+// we use this reference to select/deselect
+// after clicking the same element twice
+let selectedId
+
+// simulation setup with all forces
+const linkForce = d3
+  .forceLink()
+  .id(link => link.id)
+  .strength(link => link.strength)
 
 const simulation = d3
-	.forceSimulation()
-	.force('charge', d3.forceManyBody().strength(-20))
-	.force('center', d3.forceCenter(width / 2, height / 2));
+  .forceSimulation()
+  .force('link', linkForce)
+  .force('charge', d3.forceManyBody().strength(-120))
+  .force('center', d3.forceCenter(width / 2, height / 2))
 
-function getNodeColor(node) {
-	return node.level === 1 ? 'red' : 'blue';
+const dragDrop = d3.drag().on('start', (node) => {
+  node.fx = node.x
+  node.fy = node.y
+}).on('drag', (node) => {
+  simulation.alphaTarget(0.7).restart()
+  node.fx = d3.event.x
+  node.fy = d3.event.y
+}).on('end', (node) => {
+  if (!d3.event.active) {
+    simulation.alphaTarget(0)
+  }
+  node.fx = null
+  node.fy = null
+})
+
+/**---------------------------
+ --- UPDATE & INTERACTION ---
+ ---------------------------**/
+
+// select node is called on every click
+// we either update the data according to the selection
+// or reset the data if the same node is clicked twice
+function selectNode(selectedNode) {
+  if (selectedId === selectedNode.id) {
+    selectedId = undefined
+    resetData()
+    updateSimulation()
+  } else {
+    selectedId = selectedNode.id
+    updateData(selectedNode)
+    updateSimulation()
+  }
+
+  const neighbors = getNeighbors(selectedNode, baseLink)
+
+  // we modify the styles to highlight selected nodes
+  nodeElements.attr('fill', node => getNodeColor(node, neighbors))
+  textElements.attr('fill', node => getTextColor(node, neighbors))
+  linkElements.attr('stroke', link => getLinkColor(selectedNode, link))
 }
 
-const nodeElements = svg
-	.append('g')
-	.selectAll('circle')
-	.data(nodes)
-	.enter()
-	.append('circle')
-	.attr('r', 10)
-	.attr('fill', getNodeColor);
+// this helper simple adds all nodes and links
+// that are missing, to recreate the initial state
+function resetData() {
+  const nodeIds = nodes.map(node => node.id)
 
-const textElements = svg
-	.append('g')
-	.selectAll('text')
-	.data(nodes)
-	.enter()
-	.append('text')
-	.text((node) => node.label)
-	.attr('font-size', 15)
-	.attr('dx', 15)
-	.attr('dy', 4);
+  baseNodes.forEach((node) => {
+    if (nodeIds.indexOf(node.id) === -1) {
+      nodes.push(node)
+    }
+  })
 
-const linkElements = svg
-	.append('g')
-	.selectAll('line')
-	.data(links)
-	.enter()
-	.append('line')
-	.attr('stroke-width', 1)
-	.attr('stroke', '#E5E5E5');
+  links = baseLink
+}
 
-simulation.force('link', d3
-	.forceLink()
-	.id((link) => link.id)
-	.strength((link) => link.strength)
-);
+// diffing and mutating the data
+function updateData(selectedNode) {
+  const neighbors = getNeighbors(selectedNode, baseLink)
+  const newNodes = baseNodes.filter(node => neighbors.indexOf(node.id) > -1 || node.level === 1)
 
-//simulation.force('link').link(links)
+  const diff = {
+    removed: nodes.filter(node => newNodes.indexOf(node) === -1),
+    added: newNodes.filter(node => nodes.indexOf(node) === -1)
+  }
 
-simulation.nodes(nodes).on('tick', () => {
-	nodeElements.attr('cx', (node) => node.x).attr('cy', (node) => node.y);
-	textElements.attr('x', (node) => node.x).attr('y', (node) => node.y);
+  diff.removed.forEach(node => nodes.splice(nodes.indexOf(node), 1))
+  diff.added.forEach(node => nodes.push(node))
 
-	linkElements
-		.attr('x1', (link) => link.source.x)
-		.attr('y1', (link) => link.source.y)
-		.attr('x2', (link) => link.target.x)
-		.attr('y2', (link) => link.target.y);
-});
+  links = baseLink.filter(link => link.target.id === selectedNode.id || link.source.id === selectedNode.id)
+}
+
+function updateGraph() {
+  // links
+  linkElements = linkGroup.selectAll('line').data(links, link => link.target.id + link.source.id)
+  linkElements.exit().remove()
+
+  const linkEnter = linkElements.enter().append('line').attr('stroke-width', 1).attr('stroke', 'rgba(50, 50, 50, 0.2)')
+
+  linkElements = linkEnter.merge(linkElements)
+
+  // nodes
+  nodeElements = nodeGroup.selectAll('circle').data(nodes, node => node.id)
+  nodeElements.exit().remove()
+
+  const nodeEnter = nodeElements
+    .enter()
+    .append('circle')
+    .attr('r', 10)
+    .attr('fill', node => node.level === 1 ? 'red' : 'gray')
+    .call(dragDrop)
+    // we link the selectNode method here
+    // to update the graph on every click
+    .on('click', selectNode)
+
+  nodeElements = nodeEnter.merge(nodeElements)
+
+  // texts
+  textElements = textGroup.selectAll('text').data(nodes, node => node.id)
+  textElements.exit().remove()
+
+  const textEnter = textElements
+    .enter()
+    .append('text')
+    .text(node => node.label)
+    .attr('font-size', 15)
+    .attr('dx', 15)
+    .attr('dy', 4)
+
+  textElements = textEnter.merge(textElements)
+}
+
+function updateSimulation() {
+  updateGraph()
+
+  simulation.nodes(nodes).on('tick', () => {
+    nodeElements.attr('cx', node => node.x).attr('cy', node => node.y)
+    textElements.attr('x', node => node.x).attr('y', node => node.y)
+    linkElements
+      .attr('x1', link => link.source.x)
+      .attr('y1', link => link.source.y)
+      .attr('x2', link => link.target.x)
+      .attr('y2', link => link.target.y)
+  })
+
+  simulation.force('link').links(links)
+  simulation.restart()
+}
+
+// last but not least, we call updateSimulation
+// to trigger the initial render
+updateSimulation()
