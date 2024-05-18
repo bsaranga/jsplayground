90b5b92ea59c957bc03d9e838980a988cd438c55
Buwaneka Saranga <bsaranga@gmail.com>
2024-05-18 06:40:04 +0530

    chore: Update canvas API code

--- canvas_api/index.js
+++ canvas_api/index.js
@@ -1,27 +1,4 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-
-let done = false;
-let frameCount = 0;
-
-function step(timestamp) {
-
-    console.log(timestamp);
-    frameCount++;
-
-    if (timestamp > 500) {
-        done = true;
-        const fps = frameCount / (timestamp/1000);
-        console.log(`FPS: ${fps}`);
-    }
-
-    if (!done) {
-        window.requestAnimationFrame(step);
-    }
-}
-
-const animationId = window.requestAnimationFrame(step);
-
-
-console.log(animationId)
+console.log(ctx);
